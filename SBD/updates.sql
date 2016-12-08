846e9450de707efd5ec0eedf2ff923ea4cca4e19
Gustavo Silva <xgustavossilva@gmail.com>
2016-12-08 19:21:25 -0200

    Finalizado os updates e selects necessarios

--- SBD/updates.sql
+++ SBD/updates.sql
@@ -5,9 +5,9 @@ SELECT insereAluno('15363987810','Thomas Aquino','1961-06-16','thomas@globlo.com
 --Remoção neste caso vale a pena lembrar que todas as pessoas, sendo ela aluno, professor, etc, ao serem removidas elas devem também ser removidas do banco de pessoas
 --Neste caso foi criado um trigger para tratar disso, então todas as remoções aqui já são de mais de uma tabela.
 DELETE FROM aluno WHERE nome = 'Jose';
-DELETE FROM unidade WHERE sigla = 'FEELT' CASCADE;
+DELETE FROM tecnico WHERE nome = 'Alex';
 DELETE FROM professor WHERE cpf = '62345678901';
 
 UPDATE pessoa SET nome = 'José Lemes' WHERE nome = 'Jose';
 UPDATE unidade SET nome = 'Faculdade de Computação' WHERE sigla = 'FACOM';
--- PENSAR NO TERCEIRO UPDATE
+UPDATE aluno SET curso = (SELECT sigla FROM curso WHERE nome = 'Bacha. em Sistema da Inf.') WHERE curso = 'BCC'
